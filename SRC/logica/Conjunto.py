babcb62a6638a0c338157feff5c72b6c9844d495
labf402 <73878026@continental.edu.pe>
2024-10-15 16:35:53 -0500

    Caso4 :Calcular promedio con un conjunto de n elementos (Rojo)

--- SRC/logica/Conjunto.py
+++ SRC/logica/Conjunto.py
@@ -1,7 +1,12 @@
 class Conjunto:
-
     def __init__(self, conjunto):
         self.__conjunto=conjunto
-
-    def promedio(self):
-        return None
+    def promedio( self ):
+        if len(self.__conjunto) == 1:
+            return (self.__conjunto[0])
+        elif len(self.__conjunto) == 2:
+            return (self.__conjunto[0]+self.__conjunto[1])/2
+          elif len(self.__conjunto) > 2:
+            return sum(self.__conjunto)/len(self.__conjunto)
+        else:
+             return None
--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,6 +1,16 @@
 import unittest
 from SRC.logica.Conjunto import Conjunto
-class TestConjunto( unittest.TestCase):
-    def test_conjunto_vacio_retornaNone(self):
-        conjunto = Conjunto([])
+class TestConjunto( unittest.TestCase ):
+    def test_conjunto_vacio_retornaNone( self ):
+        conjunto=Conjunto([])
         self.assertIsNone(conjunto.promedio())
+    def test_conjunto_unElemento_retornaValorUnicoElemento( self ):
+        conjunto=Conjunto([5])
+        self.assertEqual(5,conjunto.promedio())
+    def test_conjunto_dosElementos_retornaPromedioElementos( self ):
+        conjunto=Conjunto([5,7])
+        self.assertEqual(6,conjunto.promedio())
+
+    def test_conjunto_nElementos_retornaPromedioNElementos( self ):
+        conjunto=Conjunto([2,4,8,9,10,15])
+        self.assertEqual((2+4+8+9+10+15)/6,conjunto.promedio())
